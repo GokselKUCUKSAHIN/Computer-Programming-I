4e38c642208a157ffa097776e2f26330099e99e6
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-07-10 06:59:48 +0300

    Update and rename sinus_hesaplama.c to main.c

--- C - Alper BASTURK/Project_3/main.c
+++ C - Alper BASTURK/Project_3/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h> //Matematik kütüphanesi, sin() komutu için gerekli.
+#include <math.h> //Matematik kÃ¼tÃ¼phanesi, sin() komutu iÃ§in gerekli.
 
 int main(void)
 {
     float aci;
     printf("acinin derece cinsinden degerini giriniz -> ");
     scanf("%f",&aci);
-    float radyan=aci*(22/7.0)/180; //Raydan = Pi/180, Pi 22/7 olarak yaklaşık hesaplıyoruz
+    float radyan=aci*(22/7.0)/180; //Raydan = Pi/180, Pi 22/7 olarak yaklaÅŸÄ±k hesaplÄ±yoruz
     printf("\n%7.2f derecenin sinusu %5.3f\'dir.\n\n",aci,sin(radyan));
     system("PAUSE");
     return 0;
-} 
+}
