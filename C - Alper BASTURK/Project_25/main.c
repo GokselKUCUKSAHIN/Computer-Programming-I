7cba4e4f4af3f9309b0592e283ff3ad7f34f1723
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-07-10 06:54:14 +0300

    Update main.c

--- C - Alper BASTURK/Project_25/main.c
+++ C - Alper BASTURK/Project_25/main.c
@@ -6,17 +6,16 @@ void takas(int *,int *);
 int main(void) {
 	int a,b;
 	printf("Takas edilecek sayilari giriniz -> ");
-	scanf("%d %d",&a,&b);
-	printf("Takas oncesi : a=%d b=%d\n",a,b);
-	takas(&a,&b);
-	printf("Takas sonrasi: a=%d b=%d\n",a,b);
+	scanf("%d %d", &a,&b);
+	printf("Takas oncesi : a=%d b=%d\n", a, b);
+	takas(&a, &b);
+	printf("Takas sonrasi: a=%d b=%d\n", a, b);
 	system("PAUSE");
 	return 0;
 }
 void takas(int *x,int *y)
 {
-	int z;
-	z= *x;
-	*x=*y;
-	*y=z;
+	int tmp = *x;
+	*x = *y;
+	*y = z;
 }
